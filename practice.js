cb0a86ed9f6479558105b12b7692023152e783bc
Jordan Glad <jordanglad8@gmail.com>
2021-11-08 16:48:54 -0600

    Why isn't it working

---
+++ practice.js
@@ -0,0 +1,8 @@
+function timesTwo(num) {
+    return num * 2;
+}
+console.log(timesTwo(69));
+var divideByThree = function (num) {
+    return num / 3;
+};
+console.log(divideByThree(69));
---
+++ practice.ts
@@ -0,0 +1,10 @@
+function timesTwo(num: number): number{
+    return num * 2;
+}
+console.log(timesTwo(69));
+
+function divideByThree(num: number): number {
+    return num / 3;
+}
+
+console.log(divideByThree(69));
